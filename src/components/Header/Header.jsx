adb173e49dce4cb98bf5eb1dfd21ee199eb3cbee
juan rojas <jrojasbf@gmail.com>
2022-06-16 21:42:08 -0700

    added boilerplate

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+
+import "./Header.scss"
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
+function Header() {
+    return (
+        <Navbar bg="dark" variant="dark" sticky="top">
+            <div className="hdr">
+                <Navbar.Brand href="/">Home</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="#Information">Information</Nav.Link>
+                    <Nav.Link href="#Register">Register</Nav.Link>
+                    <Nav.Link href="#Follow">Follow Us</Nav.Link>
+                </Nav>
+            </div>
+        </Navbar>
+
+    )
+}
+
+export default Header
