6bd7383bf872d67c7001c207a0aed5521feeeaa4
juan rojas <jrojasbf@gmail.com>
2022-06-17 17:52:29 -0700

    information tbd

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import './App.scss';
 import Header from './components/Header/Header';
 import Information from './components/Information/Information';
+import Footer from './components/Footer/Footer';
+// import Register from './components/Register/Register';
 // import { Route } from 'react-router-dom';
 
 function App() {
@@ -10,6 +12,9 @@ function App() {
     <div className='App'>
       <Header />
       <Information />
+      {/* <Register /> */}
+      <Footer />
+
 
     </div>
 
--- src/components/Information/Information.jsx
+++ src/components/Information/Information.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import "./Information.scss"
+import bglogo from "../../assets/images/bg.png";
 
 function Information() {
     return (
-        <div className='a'>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam esse dolore ex dignissimos non sapiente alias minus sit labore totam doloremque, repudiandae hic perferendis iure repellat ea aliquid, fugit doloribus?</p>
+        <div className="i" id="Home">
+            <div className="i-left">
+                <div className="i-left-wrapper">
+                    <h1 className="i-name">Welcome to the Bell Gardens Grand Prix</h1>
+                    <p className="i-description">
+                        The Bell Gardens GP is Bell Gardens newest yugioh tournament.
+                        As high schoolers we did not have the opportunity to play a tournament that was
+                        in the city of Bell Gardens. As a result we always had to duel within our own small circle of friends.
+                        Now it is our turn to host these tournaments and give others a chance to experience something we were not able to.
+
+                    </p>
+
+                    <h3>Registration and more information coming soon...</h3>
+                </div>
+            </div>
+
+            <div className="i-right">
+
+                <img src={bglogo} className="i-img" alt="logo" />
+
+            </div>
+
+
         </div>
     )
 }
